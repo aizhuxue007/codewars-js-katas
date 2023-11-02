4746688742b3dcf447782c25dd16881e1b7d309f
aizhuxue007 <aizhuprogram@gmail.com>
2023-11-02 11:46:43 -0400

    bmi

--- index.js
+++ index.js
@@ -109,3 +109,18 @@ class Solution{
     console.log('Hello World!')
   }
 }
+
+function bmi(weight, height) {
+  var formula = (weight / Math.pow(height, 2));
+  switch (true) {
+    case formula <=18.5:
+    return 'Underweight';
+    case formula <=25.0:
+    return 'Normal';
+    case formula <=30:
+    return 'Overweight';
+    default:
+    return 'Obese';
+    
+  }
+}
