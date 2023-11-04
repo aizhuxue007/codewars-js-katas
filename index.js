f01d6d5f8825f181b22f88247c4ff6c1d4928ac6
aizhuxue007 <aizhuprogram@gmail.com>
2023-11-03 23:02:25 -0400

    alternating case prototype

--- index.js
+++ index.js
@@ -124,3 +124,7 @@ function bmi(weight, height) {
     
   }
 }
+
+String.prototype.toAlternatingCase = function () {
+  return this.split('').map(c => c == c.toUpperCase() ? c.toLowerCase() : c.toUpperCase()).join('')
+}
