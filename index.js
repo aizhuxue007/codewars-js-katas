b2bd284d7c89826e9527ffd5963e82d63f707bd9
Aizhu Xue <aizhudeveloper@gmail.com>
2023-11-17 07:13:22 -0500

    dating age

--- index.js
+++ index.js
@@ -185,3 +185,17 @@ function addExtra( listOfNumbers ){
   newList.push(1)
   return newList;
 }
+
+function datingRange(age){
+  let min, max;
+  
+  if (age <= 14) {
+    min = Math.floor(age - 0.10 * age);
+    max = Math.floor(age + 0.10 * age);
+  } else {
+    min = Math.floor(age / 2) + 7;
+    max = Math.floor(age - 7) * 2;
+  }
+  
+  return `${min}-${max}`;
+}
