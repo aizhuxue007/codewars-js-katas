e9b2929714878ddeea6f658a30d747e73067d1d8
Aizhu Xue <aizhudeveloper@gmail.com>
2023-11-18 08:22:57 -0500

    feast

--- index.js
+++ index.js
@@ -199,3 +199,7 @@ function datingRange(age){
   
   return `${min}-${max}`;
 }
+
+function feast(beast, dish) {
+  return beast.charAt(0) === dish.charAt(0) && beast.charAt(beast.length - 1) == dish.charAt(dish.length - 1);
+}
