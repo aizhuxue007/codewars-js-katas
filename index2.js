7733ecb6ae8064cf5063f50cb53c770083f21eac
Aizhu Xue <aizhudeveloper@gmail.com>
2023-11-26 08:01:26 -0500

    move

--- index2.js
+++ index2.js
@@ -29,3 +29,7 @@ var Ghost = function() {
   let colors = ['white', 'yellow', 'purple', 'red'];
   this.color = colors[Math.floor(Math.random() * 4)];
 }
+
+function move (position, roll) {
+  return position + roll * 2;
+}
