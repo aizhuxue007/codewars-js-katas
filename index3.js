7c0eb5ef072cee43461be7b181cc46bf0b4d0c99
Aizhu Xue <aizhudeveloper@gmail.com>
2023-12-09 06:28:07 -0500

    htmlspecial chars

--- index3.js
+++ index3.js
@@ -1,3 +1,10 @@
 function sumOfDifferences(arr) {
     return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
 }
+
+function htmlspecialchars(formData) {
+    return formData.replace(/&/g, '&amp;')
+                   .replace(/</g, '&lt;')
+                   .replace(/>/g, '&gt;')
+                   .replace(/"/g, '&quot;')      
+  }
