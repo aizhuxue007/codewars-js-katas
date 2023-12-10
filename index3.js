3744c986b3df70872a96de3e2c2c64a4cc4b8bb0
Aizhu Xue <aizhudeveloper@gmail.com>
2023-12-10 06:10:37 -0500

    isPythagoreanTriple

--- index3.js
+++ index3.js
@@ -1,10 +1,16 @@
 function sumOfDifferences(arr) {
-    return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+  return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
 }
 
 function htmlspecialchars(formData) {
-    return formData.replace(/&/g, '&amp;')
-                   .replace(/</g, '&lt;')
-                   .replace(/>/g, '&gt;')
-                   .replace(/"/g, '&quot;')      
+  return formData
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;");
+}
+
+function isPythagoreanTriple(integers) {
+    let orderedInts = integers.sort((a, b) => a - b)
+    return orderedInts[0] ** 2 + orderedInts[1] ** 2 === orderedInts[2] ** 2;
   }
