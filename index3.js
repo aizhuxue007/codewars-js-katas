17f8a55c7c90845c9f4351d51d505b9fa4da3f9e
Aizhu Xue <aizhudeveloper@gmail.com>
2023-12-11 19:43:06 -0500

    closecompare

--- index3.js
+++ index3.js
@@ -14,3 +14,7 @@ function isPythagoreanTriple(integers) {
     let orderedInts = integers.sort((a, b) => a - b)
     return orderedInts[0] ** 2 + orderedInts[1] ** 2 === orderedInts[2] ** 2;
   }
+
+  function closeCompare(a, b, m = 0){
+    return Math.abs(a - b) <= m ? 0 : Math.sign(a - b);
+  }
