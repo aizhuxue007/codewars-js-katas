6a0fb8451909061638c5a44311d0b6be8373e4bc
Aizhu Xue <aizhudeveloper@gmail.com>
2023-12-13 07:16:50 -0500

    cube 2

--- index3.js
+++ index3.js
@@ -30,3 +30,15 @@ class Ship {
    }
 }
 
+class Cube {
+  constructor(side) {
+    this.setSide(side)
+  }
+  getSide() {
+    return this.side; 
+  }
+  setSide(side = 0) {
+    this.side = Math.abs(side);
+    
+  }
+}
