e8736d376987e8fac2b4bbb947a1da5627078607
Aizhu Xue <aizhudeveloper@gmail.com>
2023-12-27 07:15:12 -0500

    evil

--- index4.js
+++ index4.js
@@ -2,3 +2,7 @@ function remove(s, n) {
   for (let i = 0; i < n; i++) s = s.replace("!", "");
   return s;
 }
+
+function evil(n) {
+    return n.toString(2).split('').filter(binDigit => binDigit === '1').length % 2 === 0 ? "It's Evil!" : "It's Odious!"
+}
