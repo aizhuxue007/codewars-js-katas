8181c843619612639762758918f3491ebc601787
Aizhu Xue <aizhudeveloper@gmail.com>
2023-12-28 06:29:24 -0500

    esrever

--- index4.js
+++ index4.js
@@ -6,3 +6,7 @@ function remove(s, n) {
 function evil(n) {
     return n.toString(2).split('').filter(binDigit => binDigit === '1').length % 2 === 0 ? "It's Evil!" : "It's Odious!"
 }
+
+function esrever(s){
+  return s.slice(0, -1).split('').reverse().join + s.slice(-1)
+}
