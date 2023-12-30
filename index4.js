f25d7992424ac725735661d80152309f720786a3
Aizhu Xue <aizhudeveloper@gmail.com>
2023-12-30 06:34:37 -0500

    cubechecker

--- index4.js
+++ index4.js
@@ -4,15 +4,19 @@ function remove(s, n) {
 }
 
 function evil(n) {
-    return n.toString(2).split('').filter(binDigit => binDigit === '1').length % 2 === 0 ? "It's Evil!" : "It's Odious!"
+  return n.toString(2).split('').filter(binDigit => binDigit === '1').length % 2 === 0 ? "It's Evil!" : "It's Odious!"
 }
 
-function esrever(s){
+function esrever(s) {
   return s.slice(0, -1).split('').reverse().join + s.slice(-1)
 }
 
-var humanYearsCatYearsDogYears = function(humanYears) {
+var humanYearsCatYearsDogYears = function (humanYears) {
   if (humanYears === 1) return [1, 15, 15]
   if (humanYears === 2) return [2, 24, 24]
   return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24]
 }
+
+var cubeChecker = function (volume, side) {
+  return side ** 3 === volume && side > 0;
+};
