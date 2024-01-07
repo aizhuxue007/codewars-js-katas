ef2a891a59f1bab7a43951446dbd5b6936c9ad51
Aizhu Xue <aizhudeveloper@gmail.com>
2024-01-07 02:17:09 -0500

    warn the sheep

--- index4.js
+++ index4.js
@@ -4,56 +4,67 @@ function remove(s, n) {
 }
 
 function evil(n) {
-  return n.toString(2).split('').filter(binDigit => binDigit === '1').length % 2 === 0 ? "It's Evil!" : "It's Odious!"
+  return n
+    .toString(2)
+    .split("")
+    .filter((binDigit) => binDigit === "1").length %
+    2 ===
+    0
+    ? "It's Evil!"
+    : "It's Odious!";
 }
 
 function esrever(s) {
-  return s.slice(0, -1).split('').reverse().join + s.slice(-1)
+  return s.slice(0, -1).split("").reverse().join + s.slice(-1);
 }
 
 var humanYearsCatYearsDogYears = function (humanYears) {
-  if (humanYears === 1) return [1, 15, 15]
-  if (humanYears === 2) return [2, 24, 24]
-  return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24]
-}
+  if (humanYears === 1) return [1, 15, 15];
+  if (humanYears === 2) return [2, 24, 24];
+  return [humanYears, (humanYears - 2) * 4 + 24, (humanYears - 2) * 5 + 24];
+};
 
 var cubeChecker = function (volume, side) {
   return side ** 3 === volume && side > 0;
 };
 
 function rainAmount(mm) {
-  if (mm < 40)
-    return `You need to give your plant ${40 - mm}mm of water`
-  return `Your plant has had more than enough water for today!`
+  if (mm < 40) return `You need to give your plant ${40 - mm}mm of water`;
+  return `Your plant has had more than enough water for today!`;
 }
 
 function calculateAge(birthYear, relationYear) {
   let years;
   if (relationYear > birthYear) {
-    years = relationYear - birthYear
-    return `You are ${years} year${years > 1 ? 's' : ''} old.`
+    years = relationYear - birthYear;
+    return `You are ${years} year${years > 1 ? "s" : ""} old.`;
   } else if (birthYear > relationYear) {
-    years = birthYear - relationYear
-    return `You will be born in ${years} year${years > 1 ? 's' : ''}.`
+    years = birthYear - relationYear;
+    return `You will be born in ${years} year${years > 1 ? "s" : ""}.`;
   } else {
     return `You were born this very year!`;
   }
 }
 
 function alphabetPosition(text) {
-  return text.split('').filter(char => char.toLowerCase().match(/[a-z]/i) ? char.toLowerCase() : '').map(letter => letter.toLowerCase().charCodeAt(0) - 96)
-    .join(' ');
+  return text
+    .split("")
+    .filter((char) =>
+      char.toLowerCase().match(/[a-z]/i) ? char.toLowerCase() : ""
+    )
+    .map((letter) => letter.toLowerCase().charCodeAt(0) - 96)
+    .join(" ");
 }
 
 function fuelPrice(litres, pricePerLitre) {
-  let discount
-  if (litres > 8) discount = 0.25
-  else if (litres >= 6) discount = 0.20
-  else if (litres >= 4) discount = 0.10
-  else if (litres >= 2) discount = 0.05
-  else discount = 0
+  let discount;
+  if (litres > 8) discount = 0.25;
+  else if (litres >= 6) discount = 0.2;
+  else if (litres >= 4) discount = 0.1;
+  else if (litres >= 2) discount = 0.05;
+  else discount = 0;
   // return total cost of litres times (pricePerLitre - discount), rounded to nearest 2 decimal places
-  return parseFloat((litres * (pricePerLitre - discount)).toFixed(2))
+  return parseFloat((litres * (pricePerLitre - discount)).toFixed(2));
 }
 
 /** Best solution for fuelPrice */
@@ -66,23 +77,32 @@ function fuelPrice(litres, pricePerLitre) {
 //   return Math.round(litres * pricePerLiter * 100) / 100;;
 // }
 
-var quote = function(fighter) {
-    if (fighter.toLowerCase() === "george saint pierre") return "I am not impressed by your performance.";
-    if (fighter.toLowerCase() === "conor mcgregor") return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+var quote = function (fighter) {
+  if (fighter.toLowerCase() === "george saint pierre")
+    return "I am not impressed by your performance.";
+  if (fighter.toLowerCase() === "conor mcgregor")
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
 };
 
 function toCsvText(array) {
-  return array.join('\n')
+  return array.join("\n");
 }
 
-function pipeFix(numbers){
-    let stop = Math.max(...numbers)
-    let start = Math.min(...numbers)
-    let result = []
-      
-    for (let i = start; i <= stop; i++) {
-      result.push(i)
-    }
-    
-    return result
+function pipeFix(numbers) {
+  let stop = Math.max(...numbers);
+  let start = Math.min(...numbers);
+  let result = [];
+
+  for (let i = start; i <= stop; i++) {
+    result.push(i);
+  }
+
+  return result;
+}
+
+function warnTheSheep(queue) {
+  const position = queue.reverse().indexOf("wolf");
+  return position === 0
+    ? `Pls go away and stop eating my sheep`
+    : `Oi! Sheep number ${position}! You are about to be eaten by a wolf!`;
 }
