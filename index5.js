9117e90ddec67bf39dcf766ec4391959cf3eed12
Aizhu Xue <aizhudeveloper@gmail.com>
2024-01-09 06:15:28 -0500

    slope

--- index5.js
+++ index5.js
@@ -4,3 +4,10 @@ function leo(oscar) {
   if (oscar < 88) return "When will you give Leo an Oscar?";
   if (oscar > 88) return "Leo got one already!";
 }
+
+function slope(points) {
+    const rise = points[1] - points[3]
+    const run = points[0] - points[2]
+    if (run == 0) return 'undefined'
+    return String(rise / run)
+  }
