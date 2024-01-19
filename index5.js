f11d49b07cf6ba9ec4600e10dc90483b16e3a9df
Aizhu Xue <aizhudeveloper@gmail.com>
2024-01-19 11:38:20 -0500

    likes

--- index5.js
+++ index5.js
@@ -90,3 +90,13 @@ function disemvowel(str) {
   const consonants = /[^aeiou]/gi
   return str.match(consonants).join('')
 }
+
+function likes(names) {
+  switch (names.length) {
+    case 0: return "no one likes this"
+    case 1: return `${names[0]} likes this`
+    case 2: return `${names[0]} and ${names[1]} like this`
+    case 3: return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    default: return `${names[0]}, ${names[1]} and ${names.slice(2).length} others like this`
+  }
+}
