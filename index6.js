6e5af77d76ca3a734180f02c7184a0f3365cb6b3
Aizhu Xue <aizhudeveloper@gmail.com>
2024-01-21 05:47:14 -0500

    scrollingText

--- index6.js
+++ index6.js
@@ -1,8 +1,20 @@
-function shorter_reverse_longer(a,b) {
-    const aLen = a.length
-    const bLen = b.length
-    const shorter = aLen < bLen ? a : b;
-    const longer = aLen > bLen ? a : b;
-    if (aLen == bLen) return b + a.split('').reverse().join('') + b;
-    return shorter + longer.split('').reverse().join('') + shorter;
+function shorter_reverse_longer(a, b) {
+  const aLen = a.length;
+  const bLen = b.length;
+  const shorter = aLen < bLen ? a : b;
+  const longer = aLen > bLen ? a : b;
+  if (aLen == bLen) return b + a.split("").reverse().join("") + b;
+  return shorter + longer.split("").reverse().join("") + shorter;
+}
+
+function scrollingText(text) {
+  const result = [];
+  let permutation = text.split("");
+
+  for (let i = 0; i < text.length; i++) {
+    result.push(permutation.join("").toUpperCase());
+    permutation.push(permutation.shift());
+  }
+
+  return result;
 }
