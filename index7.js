38da5a605d78925368040622574304b0876b850a
Aizhu Xue <aizhudeveloper@gmail.com>
2024-01-29 18:55:18 -0500

    wordsToMakrs

--- index7.js
+++ index7.js
@@ -4,3 +4,7 @@ function maxProduct(numbers, size) {
     .slice(0, size)
     .reduce((acc, n) => acc * n);
 }
+
+function wordsToMarks(string) {
+  return [...string].reduce((acc, curr) => (acc += curr.charCodeAt(0) - 96), 0);
+}
