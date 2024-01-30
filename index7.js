6b70629bd4619e89cf099133a214b1cdfd7c1d44
Aizhu Xue <aizhudeveloper@gmail.com>
2024-01-30 07:11:13 -0500

    factorial

--- index7.js
+++ index7.js
@@ -8,3 +8,13 @@ function maxProduct(numbers, size) {
 function wordsToMarks(string) {
   return [...string].reduce((acc, curr) => (acc += curr.charCodeAt(0) - 96), 0);
 }
+
+function factorial(n) {
+  if (n < 0 || n > 12) throw new RangeError();
+  if (n == 0 || n == 1) return 1;
+  let product = 1;
+  for (let i = n; i > 1; i--) {
+    product *= i;
+  }
+  return product;
+}
