13675fe54993d4bc067be36b7c8e2f884ee7417e
Aizhu Xue <aizhudeveloper@gmail.com>
2024-02-01 03:45:46 -0500

    howMuchIloveYou

--- index7.js
+++ index7.js
@@ -26,3 +26,16 @@ function sumArray(array) {
         Math.min(...array)
     : 0;
 }
+
+const phrases = [
+    'I love you',
+    'a little',
+    'a lot',
+    'passionately',
+    'madly',
+    'not at all',
+]
+
+function howMuchILoveYou(n) {
+  return phrases[(n - 1) % phrases.length]
+}
