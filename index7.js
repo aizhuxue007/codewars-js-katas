7006ecef9d88022b467f99fd75945b6f2262ba53
Aizhu Xue <aizhudeveloper@gmail.com>
2024-02-07 07:08:14 -0500

    math

--- index7.js
+++ index7.js
@@ -77,3 +77,27 @@ function hexHash(code){
     return acc += n
   }, 0)
 }
+
+function add(a,b){
+  return a + b
+}
+
+function divide(a,b){
+  return a / b
+}
+
+function multiply(a,b){
+  return a * b
+}
+
+function mod(a,b){
+  return a % b
+}
+ 
+function exponent(a,b){
+  return a ** b
+}
+  
+function subt(a,b){
+  return a - b
+}
