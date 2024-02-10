4f0a7f54fc8d0937558a77fae8a8826e5e283a14
Aizhu Xue <aizhudeveloper@gmail.com>
2024-02-10 06:22:35 -0500

    isPangram

--- index8.js
+++ index8.js
@@ -1,3 +1,12 @@
 function boolToWord(bool) {
     return bool ? 'Yes' : 'No';
 }
+
+function isPangram(string) {
+    const normalizedStr = string.toLowerCase()
+    for (let charCode = 97; charCode <= 122; charCode++) {
+        const char = String.fromCharCode(charCode)
+        if (!normalizedStr.includes(char)) return false
+    }
+    return true
+}
