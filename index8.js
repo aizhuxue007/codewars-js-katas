a38586edb41caba7005c788b876f09a568fea67e
Aizhu Xue <aizhudeveloper@gmail.com>
2024-02-20 10:45:40 -0500

    fizzBuzzCuckooClock

--- index8.js
+++ index8.js
@@ -69,3 +69,26 @@ function rounders(value) {
   }
   return Number(digits.reverse().join(""));
 }
+
+function rounders(value, e = 1) {
+  return value < 10 ? value * e : rounders(Math.round(value / 10), e * 10);
+}
+
+function fizzBuzzCuckooClock(time) {
+    const [ hours, minutes ] = time.split(':')
+    if (minutes === '30') { return 'Cuckoo'}
+    else if (minutes != '00') {
+      if (minutes%3 == 0 && minutes%5 == 0) return 'Fizz Buzz'
+      else if (minutes%3 == 0) return 'Fizz'
+      else if (minutes%5 == 0 ) return 'Buzz'
+      else return 'tick'
+    } else {
+      let result = ''
+      let cuckoos = hours%12 == 0 ? 12 : hours%12
+      while (cuckoos > 0) {
+         result += 'Cuckoo '
+         cuckoos--
+      }
+      return result.trim()
+    }
+  }
