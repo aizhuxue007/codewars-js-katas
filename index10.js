da0744ff3bb309fcf0e94e446350683ddd130754
Aizhu Xue <aizhudeveloper@gmail.com>
2024-03-05 07:31:09 -0500

    momentOfTimeInSpace

--- index10.js
+++ index10.js
@@ -2,3 +2,10 @@ function decode(code, n) {
     const digitKey = String(n).split('').map(c => Number(c))
     return code.map((n, i) => String.fromCharCode(n - digitKey[i % digitKey.length] + 96)).join('')
 }
+
+function momentOfTimeInSpace(moment) {
+    const momentArr = moment.split('')
+    const time = momentArr.reduce((acc, curr) => Number(curr) ? acc += Number(curr) : acc += 0, 0)
+    const space = momentArr.filter(c => !Number(c) ? true : false).length
+    return [time < space, time == space, time > space]
+}
--- index9.js
+++ index9.js
@@ -104,4 +104,3 @@ function trilingualDemocracy(group) {
 //   })
 //   return answer ? answer : Object.keys(languages).filter(l => languages[l] == 0)[0]
 // }
-
