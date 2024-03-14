eaa5f50222a98f219c78286df9d244efa00aad95
Aizhu Xue <aizhudeveloper@gmail.com>
2024-03-14 06:11:02 -0400

    reverseMiddle

--- index11.js
+++ index11.js
@@ -26,24 +26,31 @@ function mnLCM(m, n) {
 }
 
 function findShort(s) {
-  const wordLengths = s.split(' ').map(word => word.length)
-  return Math.min(...wordLengths)
+  const wordLengths = s.split(" ").map((word) => word.length);
+  return Math.min(...wordLengths);
 }
 
 function drawSpider(legSize, bodySize, mouth, eye) {
-    const legTypes = {
-      '1': "^ ^",
-      '2': '/\\ /\\',
-      '3': '/╲ ╱\\',
-      '4': "╱╲ ╱╲"
-    }
-    const bodyTypes = {
-      '1': "( )",
-      '2': "(( ))",
-      '3': "((( )))"
-    }
-    let legs = legTypes[legSize].split(' ')
-    let bodies = bodyTypes[bodySize].split(' ')
-    let eyes = eye.repeat((2**bodySize)/2)
-    return `${legs[0]}${bodies[0]}${eyes}${mouth}${eyes}${bodies[1]}${legs[1]}`
-  }
+  const legTypes = {
+    1: "^ ^",
+    2: "/\\ /\\",
+    3: "/╲ ╱\\",
+    4: "╱╲ ╱╲",
+  };
+  const bodyTypes = {
+    1: "( )",
+    2: "(( ))",
+    3: "((( )))",
+  };
+  let legs = legTypes[legSize].split(" ");
+  let bodies = bodyTypes[bodySize].split(" ");
+  let eyes = eye.repeat(2 ** bodySize / 2);
+  return `${legs[0]}${bodies[0]}${eyes}${mouth}${eyes}${bodies[1]}${legs[1]}`;
+}
+
+function reverseMiddle(array) {
+  let midIndex = array.length / 2 - 1;
+  return array
+    .slice(midIndex, array.length % 2 === 0 ? midIndex + 2 : midIndex + 3)
+    .reverse();
+}
