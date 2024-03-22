efa0a587c240d80bc704839808227daebc97ec06
Aizhu Xue <aizhudeveloper@gmail.com>
2024-03-22 04:28:16 -0400

    add List object

--- index12.js
+++ index12.js
@@ -1,35 +1,45 @@
 function getSectionIdFromScroll(scrollY, sizes) {
-    let accumulatedHeight = 0
-    for (let i = 0; i < sizes.length; i++) {
-        accumulatedHeight += sizes[i]
-        if (scrollY < accumulatedHeight) return i
-    }
-    return -1
+  let accumulatedHeight = 0;
+  for (let i = 0; i < sizes.length; i++) {
+    accumulatedHeight += sizes[i];
+    if (scrollY < accumulatedHeight) return i;
+  }
+  return -1;
 }
 
 function rounding(n, m) {
-    if (!(n >= 1 || n < 10 ** 9)) return null
-    if (!(m >= 3 || m < 10 ** 9)) return null
-    if (m / n === 2) return n
-    let remainder = n % m;
-    if (2 * remainder === m) {
-        return n;
-    }
+  if (!(n >= 1 || n < 10 ** 9)) return null;
+  if (!(m >= 3 || m < 10 ** 9)) return null;
+  if (m / n === 2) return n;
+  let remainder = n % m;
+  if (2 * remainder === m) {
+    return n;
+  }
 
-    let lowerMultiple = n - remainder;
-    let upperMultiple = (n - remainder) + m;
+  let lowerMultiple = n - remainder;
+  let upperMultiple = n - remainder + m;
 
-    if ((n - lowerMultiple) < (upperMultiple - n)) {
-        return lowerMultiple;
-    } else {
-        return upperMultiple;
-    }
+  if (n - lowerMultiple < upperMultiple - n) {
+    return lowerMultiple;
+  } else {
+    return upperMultiple;
+  }
 }
 
 function SeriesSum(n) {
-    for (var s = 0, i = 1; i <= n; i++) {
-        s += 1 / (3 * i - 2)
-    }
+  for (var s = 0, i = 1; i <= n; i++) {
+    s += 1 / (3 * i - 2);
+  }
+  return s.toFixed(2);
+}
 
-    return s.toFixed(2)
+function List() {
+  this.countSpecDigits = function (integersList, digitsList) {
+    let result = [];
+    for (let i = 0; i < digitsList.length; i++) {
+      let digit = digitsList[i];
+      result.push([digit, integersList.join().split(digit).length - 1]);
+    }
+    return result;
+  };
 }
