fcabda13e23039436dab7c66ab595939d7394597
Aizhu Xue <aizhudeveloper@gmail.com>
2024-03-26 06:41:25 -0400

    add canIPlay

--- index13.js
+++ index13.js
@@ -1,3 +1,8 @@
 function withoutLast(arr) {
   return arr.slice(0, -1);
 }
+
+function canIPlay(now, start, end) {
+  if (start > end) return now >= start || now < end;
+  return now >= start && now < end;
+}
