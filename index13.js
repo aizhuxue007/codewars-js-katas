3bcd5d36ebe9763f540a49bb231e95838b54bbcc
Aizhu Xue <aizhudeveloper@gmail.com>
2024-03-27 04:43:21 -0400

    add sortMyString

--- index13.js
+++ index13.js
@@ -6,3 +6,12 @@ function canIPlay(now, start, end) {
   if (start > end) return now >= start || now < end;
   return now >= start && now < end;
 }
+
+function sortMyString(S) {
+  let evens = "",
+    odds = "";
+  S.split("").forEach((c, i) => {
+    i % 2 === 0 ? (evens += c) : (odds += c);
+  });
+  return evens + " " + odds;
+}
