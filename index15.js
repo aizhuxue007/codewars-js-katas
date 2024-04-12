0b48a67e14e162450ab5933e65da6d66c74495e5
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-12 06:59:49 -0400

    add openOrSenior

--- index15.js
+++ index15.js
@@ -58,3 +58,9 @@ function replace(s) {
 }
 
 questions.forEach((question) => (question.usersAnswer = null));
+
+function openOrSenior(data) {
+  return data.map(([age, handicap]) =>
+    age > 54 && handicap > 7 ? "Senior" : "Open"
+  );
+}
