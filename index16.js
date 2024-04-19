106b18adb5f54ffd848b673dd172e9c96095a6e2
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-19 07:53:26 -0400

    pairs

--- index16.js
+++ index16.js
@@ -13,3 +13,15 @@ function spinningRings(innerMax, outerMax) {
 function cantor(nestedList) {
   return nestedList.map((r, i) => +!r[i]);
 }
+
+function pairs(ar) {
+  let count = 0;
+  let p1 = 0;
+  let p2 = 0;
+  for (let i = 0; i <= ar.length; i += 2) {
+    p1 = ar[i];
+    p2 = ar[i + 1];
+    if (Math.abs(p1 - p2) === 1) count++;
+  }
+  return count;
+}
