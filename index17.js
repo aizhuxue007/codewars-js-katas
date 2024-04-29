cae87c941de3cda8ca04f803797fbd8711096a10
Aizhu Xue <aizhudeveloper@gmail.com>
2024-04-29 18:27:30 -0400

    add solve, copypasta

--- index17.js
+++ index17.js
@@ -1,1 +1,7 @@
 Dog.prototype.bark = () => "Woof!";
+
+function solve(str, k) {
+  for (let i = 0, n = str.length - k, max = 0; i < n + k; i++) {
+    max = Math.max(max, +str.substring(i, n));
+  }
+}
