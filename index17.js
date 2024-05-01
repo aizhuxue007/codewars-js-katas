ecb51b52cfb7fb65073b005784d1015bd6f35e06
Aizhu Xue <aizhudeveloper@gmail.com>
2024-05-01 06:13:16 -0400

    add ghostBusters

--- index17.js
+++ index17.js
@@ -17,3 +17,9 @@ function changeMe(moneyIn) {
   if (!(moneyIn in acceptedMoney)) return moneyIn;
   return acceptedMoney[moneyIn];
 }
+
+function ghostBusters(building) {
+  if (!building.includes(" "))
+    return "You just wanted my autograph didn't you?";
+  return building.replaceAll(" ", "");
+}
