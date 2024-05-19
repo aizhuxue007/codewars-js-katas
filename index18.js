053b020cb626b13e1c99288d5c7cb735e1ce5f67
Aizhu Xue <aizhudeveloper@gmail.com>
2024-05-19 05:03:23 -0400

    add balancedNum

--- index18.js
+++ index18.js
@@ -18,3 +18,20 @@ function paperwork(n, m) {
 function sortGiftCode(code) {
   return code.split("").sort().join("");
 }
+
+function balancedNum(number) {
+  const strNum = String(number);
+  const strLen = strNum.length;
+  const splittedNum = strNum.split(
+    strLen % 2
+      ? strNum[Math.floor((strLen - 1) / 2)]
+      : `${strNum[Math.floor(strLen / 2 - 1)]}${strNum[Math.floor(strLen / 2)]}`
+  );
+  const leftSum = splittedNum[0]
+    .split("")
+    .reduce((acc, curr) => (acc += Number(curr)), 0);
+  const rightSum = splittedNum[1]
+    .split("")
+    .reduce((acc, curr) => (acc += Number(curr)), 0);
+  return leftSum === rightSum ? "Balanced" : "Not Balanced";
+}
