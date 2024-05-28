99aae9eac0aef6989137d8e1ff97a22a4b6f0909
Aizhu Xue <aizhudeveloper@gmail.com>
2024-05-28 09:48:15 -0400

    add abbrevName

--- index18.js
+++ index18.js
@@ -74,3 +74,8 @@ function sumStr(a, b) {
 }
 
 // const isReallyNaN = (val) => val != val;
+
+function abbrevName(name) {
+  const split = name.split(" ");
+  return `${split[0][0].toUpperCase()}.${split[1][0].toUpperCase()}`;
+}
