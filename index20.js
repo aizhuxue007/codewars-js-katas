d6af14268fbee1c842264ac81b76ba0d97ef6931
Aizhu Xue <aizhudeveloper@gmail.com>
2024-06-15 07:38:43 -0400

    add geometicseq

--- index20.js
+++ index20.js
@@ -1,3 +1,11 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   return distanceToPump <= fuelLeft * mpg;
 };
+
+function GeometricSequenceSum(a, r, n) {
+  const sequence = [a];
+  for (let i = 1; i < n; i++) {
+    sequence.push(sequence[i - 1] * r);
+  }
+  return sequence.reduce((acc, curr) => (acc += curr), 0);
+}
