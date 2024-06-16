925faf0a83bc41730bbd435d0c51ccc9fd8f5735
Aizhu Xue <aizhudeveloper@gmail.com>
2024-06-16 08:17:23 -0400

    add sentence

--- index20.js
+++ index20.js
@@ -9,3 +9,10 @@ function GeometricSequenceSum(a, r, n) {
   }
   return sequence.reduce((acc, curr) => (acc += curr), 0);
 }
+
+function sentence(arrayOfObjects) {
+  const sortedArrObjects = arrayOfObjects.sort(
+    (a, b) => Number(Object.keys(a)[0]) - Number(Object.keys(b)[0])
+  );
+  return sortedArrObjects.map((obj) => obj[Object.keys(obj)[0]]).join(" ");
+}
