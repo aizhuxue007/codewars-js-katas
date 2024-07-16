6083ae0e0a2d9db6148c434b5520212e2bdf4eac
Aizhu Xue <aizhudeveloper@gmail.com>
2024-07-16 07:27:54 -0400

    add sequenceSum

--- index22.js
+++ index22.js
@@ -9,3 +9,12 @@ function reverseLetter(str) {
 function getLargerNumbers(a, b) {
   return a.map((n, i) => Math.max(n, [b[i]]));
 }
+
+const sequenceSum = (begin, end, step) => {
+  if (begin > end) return 0;
+  let sum = 0;
+  for (let i = begin; i <= end; i += step) {
+    sum += i;
+  }
+  return sum;
+};
