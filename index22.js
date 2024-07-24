3bdf2483f0d1419c97cd4900e7c0a9e6bab971f7
Aizhu Xue <aizhudeveloper@gmail.com>
2024-07-24 07:04:41 -0400

    add collinearity

--- index22.js
+++ index22.js
@@ -67,3 +67,9 @@ const nextTrafficColor = {
 function updateLight(current) {
   return nextTrafficColor[current];
 }
+
+function collinearity(x1, y1, x2, y2) {
+  if ((x1 === 0 && y1 === 0) || (x2 === 0 && y2 === 0)) return true;
+  let k = x1 / x2 || y1 / y2;
+  return x1 === k * x2 && y1 === k * y2;
+}
