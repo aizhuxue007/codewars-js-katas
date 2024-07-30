dc203a0bd0762fbf7b938d23eb595110f8a9fb7c
Aizhu Xue <aizhudeveloper@gmail.com>
2024-07-30 07:33:11 -0400

    add Sleigh

--- index23.js
+++ index23.js
@@ -1,3 +1,9 @@
 function getNumberFromString(s) {
   return Number(s.match(/\d/gm).join(""));
 }
+
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function (name, password) {
+  return name === "Santa Claus" && password === "Ho Ho Ho!";
+};
