f6247ede40da88e281c9e9519ebeebf447bb512a
Aizhu Xue <aizhudeveloper@gmail.com>
2024-07-31 07:01:54 -0400

    add _if

--- index23.js
+++ index23.js
@@ -7,3 +7,7 @@ function Sleigh() {}
 Sleigh.prototype.authenticate = function (name, password) {
   return name === "Santa Claus" && password === "Ho Ho Ho!";
 };
+
+function _if(bool, func1, func2) {
+  return bool ? func1() : func2();
+}
