fee33f8de7301d8abeaf30af3c85ee133733ecbc
Aizhu Xue <aizhudeveloper@gmail.com>
2024-08-08 08:02:14 -0400

    add ops

--- index23.js
+++ index23.js
@@ -57,3 +57,13 @@ function swapValues() {
 function remove(string) {
   return string.replaceAll("!", "") + "!";
 }
+
+const ops = {
+  AND: (a, b) => a && b,
+  OR: (a, b) => a || b,
+  XOR: (a, b) => a !== b,
+};
+
+function logicalCalc(array, op) {
+  return array.reduce(ops[op]);
+}
