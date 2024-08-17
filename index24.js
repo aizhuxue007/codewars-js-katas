d923721f21d0c23ba0092d1fc98d2a5895b68b2b
Aizhu Xue <aizhudeveloper@gmail.com>
2024-08-17 06:05:49 -0400

    add mouthSize

--- index24.js
+++ index24.js
@@ -2,3 +2,7 @@ function stringClean(s) {
   const found = s.match(/\D/g);
   return found !== null ? found.join("") : "";
 }
+
+function mouthSize(animal) {
+  return animal.toLowerCase() === "alligator" ? "small" : "wide";
+}
