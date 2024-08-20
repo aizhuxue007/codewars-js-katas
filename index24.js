3cce5120b6f59f54fc1df2a5d230d6d25556f1ec
Aizhu Xue <aizhudeveloper@gmail.com>
2024-08-20 07:03:06 -0400

    add getGrade

--- index24.js
+++ index24.js
@@ -19,3 +19,12 @@ function twoHighest(arr) {
   const a2 = uniqueArr[1];
   return a1 !== a2 ? [a1, a2] : [a1];
 }
+
+function getGrade(s1, s2, s3) {
+  const average = (s1 + s2 + s3) / 3;
+  if (average >= 90) return "A";
+  if (average >= 80) return "B";
+  if (average >= 70) return "C";
+  if (average >= 60) return "D";
+  return "F";
+}
