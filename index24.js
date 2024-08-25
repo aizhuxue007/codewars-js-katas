1df8beac739c72f3e8847f449e725dd10e7a8b77
Aizhu Xue <aizhudeveloper@gmail.com>
2024-08-25 07:34:59 -0400

    add grader

--- index24.js
+++ index24.js
@@ -48,3 +48,11 @@ function getASCII(c) {
 function mango(quantity, price) {
   return Math.floor(quantity / 3) * 2 * price + (quantity % 3) * price;
 }
+
+function grader(score) {
+  if (score > 1 || score < 0.6) return "F";
+  if (score >= 0.9) return "A";
+  if (score >= 0.8) return "B";
+  if (score >= 0.7) return "C";
+  if (score >= 0.6) return "D";
+}
