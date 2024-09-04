153c11d648b79376faff4305df4daa819ea738ef
Aizhu Xue <aizhudeveloper@gmail.com>
2024-09-04 07:12:14 -0400

    add simpleMultiplication

--- index25.js
+++ index25.js
@@ -16,3 +16,7 @@ function validateCode(code) {
 function twoSort(s) {
   return s.sort()[0].split("").join("***");
 }
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
