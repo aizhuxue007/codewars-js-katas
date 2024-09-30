e36727d7670e57e2852c04dc14270e4f194d118f
Aizhu Xue <aizhudeveloper@gmail.com>
2024-09-30 18:25:29 -0400

    add finalGrade

--- index26.js
+++ index26.js
@@ -77,3 +77,10 @@ function isPalindrome(x) {
   const xLower = x.toLowerCase();
   return xLower === xLower.split("").reverse().join("");
 }
+
+function finalGrade(exam, projects) {
+  if (exam > 90 || projects > 10) return 100;
+  if (exam > 75 && projects >= 5) return 90;
+  if (exam > 50 && projects >= 2) return 75;
+  return 0;
+}
