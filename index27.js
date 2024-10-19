a3f31510f1123dffb8a732b97c1dae339a2b0a1d
Aizhu Xue <aizhudeveloper@gmail.com>
2024-10-19 07:36:55 -0400

    add switchItUp

--- index27.js
+++ index27.js
@@ -65,3 +65,28 @@ const square = (x) => x ** 2;
 function otherAngle(a, b) {
   return 180 - a - b;
 }
+
+function switchItUp(number) {
+  switch (number) {
+    case 0:
+      return "Zero";
+    case 1:
+      return "One";
+    case 2:
+      return "Two";
+    case 3:
+      return "Three";
+    case 4:
+      return "Four";
+    case 5:
+      return "Five";
+    case 6:
+      return "Six";
+    case 7:
+      return "Seven";
+    case 8:
+      return "Eight";
+    case 9:
+      return "Nine";
+  }
+}
