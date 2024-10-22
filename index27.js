e67fa44f6a22c91c190630cb1af5ce3776ae525d
Aizhu Xue <aizhudeveloper@gmail.com>
2024-10-21 20:50:55 -0400

    add barTriang

--- index27.js
+++ index27.js
@@ -94,3 +94,10 @@ function switchItUp(number) {
 function plural(n) {
   return !(n === 1);
 }
+
+function barTriang(p1, p2, p3) {
+  return [
+    +((p1[0] + p2[0] + p3[0]) / 3).toFixed(4),
+    +((p1[1] + p2[1] + p3[1]) / 3).toFixed(4),
+  ];
+}
