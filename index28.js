dcb88d1108910e5569c409db632474c6366bbb35
Aizhu Xue <aizhudeveloper@gmail.com>
2024-10-23 06:02:52 -0400

    add isVow

--- index28.js
+++ index28.js
@@ -1,1 +1,6 @@
 const solution = (string) => string.split(/(?=[A-Z])/).join(" ");
+
+const isVow = (a) =>
+  a.map((n) =>
+    [97, 101, 105, 111, 117].includes(n) ? String.fromCharCode(n) : n
+  );
