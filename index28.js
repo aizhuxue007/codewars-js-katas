82780d0607964ced10bd3894e0d4737b2f2ef6dd
Aizhu Xue <aizhudeveloper@gmail.com>
2024-10-24 12:43:24 -0400

    add polish to English

--- index28.js
+++ index28.js
@@ -4,3 +4,22 @@ const isVow = (a) =>
   a.map((n) =>
     [97, 101, 105, 111, 117].includes(n) ? String.fromCharCode(n) : n
   );
+
+const polishToEnglish = {
+  ą: "a",
+  ć: "c",
+  ę: "e",
+  ł: "l",
+  ń: "n",
+  ó: "o",
+  ś: "s",
+  ź: "z",
+  ż: "z",
+};
+
+function correctPolishLetters(string) {
+  return string
+    .split("")
+    .map((c) => polishToEnglish[c] || c)
+    .join("");
+}
