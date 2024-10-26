84d91cd929746723c39c914e15620a31164fb456
Aizhu Xue <aizhudeveloper@gmail.com>
2024-10-26 07:10:56 -0400

    add Person Class

--- index28.js
+++ index28.js
@@ -23,3 +23,20 @@ function correctPolishLetters(string) {
     .map((c) => polishToEnglish[c] || c)
     .join("");
 }
+
+class Person {
+  constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`;
+  }
+}
