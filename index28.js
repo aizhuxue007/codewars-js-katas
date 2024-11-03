3d0b77ec5d952ac5b6f1121ccf38967e489cf767
Aizhu Xue <aizhudeveloper@gmail.com>
2024-11-03 03:59:20 -0500

    add multiTable

--- index28.js
+++ index28.js
@@ -67,3 +67,11 @@ function helloWorld() {
   const str = "Hello World!";
   console.log(str);
 }
+
+function multiTable(number) {
+  let table = "";
+  for (let i = 1; i <= 10; i++) {
+    table += `${i} * ${number} = ${i * number}\n`;
+  }
+  return table.substring(0, table.length - 1);
+}
