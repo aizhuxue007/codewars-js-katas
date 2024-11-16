92874f06f40b1a0639621fa752b30cb7491b8882
Aizhu Xue <aizhudeveloper@gmail.com>
2024-11-16 07:05:39 -0500

    add uefaEuro2016

--- index29.js
+++ index29.js
@@ -7,3 +7,11 @@ function squareArea(A) {
 }
 
 const greet = (name, owner) => `Hello ${name === owner ? "boss" : "guest"}`;
+
+function uefaEuro2016(teams, scores) {
+  const result =
+    scores[0] === scores[1]
+      ? "teams played draw."
+      : `${scores[0] > scores[1] ? teams[0] : teams[1]} won!`;
+  return `At match ${teams[0]} - ${teams[1]}, ${result}`;
+}
