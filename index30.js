3e842c504d70814f10fec56dbf6eb99152e72db7
Aizhu Xue <aizhudeveloper@gmail.com>
2024-11-29 18:31:21 -0500

    add calculator

--- index30.js
+++ index30.js
@@ -6,3 +6,18 @@ function padIt(str, n) {
   }
   return result.join("");
 }
+
+function calculator(a, b, sign) {
+  switch (sign) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "/":
+      return a / b;
+    case "*":
+      return a * b;
+    default:
+      return "unknown value";
+  }
+}
