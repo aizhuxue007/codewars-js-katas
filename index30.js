1c31cb10d1bd1187db6314fb5df4ac6dfac3301b
Aizhu Xue <aizhudeveloper@gmail.com>
2024-12-10 03:31:38 -0500

    add switch case

--- index30.js
+++ index30.js
@@ -79,3 +79,37 @@ function playerRankUp(points) {
 function pointsPer48(ppg, mpg) {
   return ppg !== 0 && mpg !== 0 ? Number(((ppg / mpg) * 48).toFixed(1)) : 0;
 }
+
+function howManydays(month) {
+  var days;
+  switch (month) {
+    case 2:
+      days = 28;
+      break;
+    case 1:
+      days = 31;
+      break;
+    case 3:
+      days = 31;
+      break;
+    case 5:
+      days = 31;
+      break;
+    case 7:
+      days = 31;
+      break;
+    case 8:
+      days = 31;
+      break;
+    case 10:
+      days = 31;
+      break;
+    case 12:
+      days = 31;
+      break;
+    default:
+      days = 30;
+      break;
+  }
+  return days;
+}
