385dac28fee6a5a4ec323d4fc5e9f089c8e122ec
Aizhu Xue <aizhudeveloper@gmail.com>
2024-12-13 18:02:36 -0500

    add blackAndWhite

--- index31.js
+++ index31.js
@@ -5,3 +5,10 @@ function typeValidation(variable, type) {
 function odds(values) {
   return values.filter((value) => value % 2 === 1);
 }
+
+function blackAndWhite(arr) {
+  if (!Array.isArray(arr)) return "It's a fake array";
+  return `It's a ${
+    arr.indexOf(5) !== -1 && arr.indexOf(13) !== -1 ? "black" : "white"
+  } array`;
+}
