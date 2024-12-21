a6ee90a0947b5ef8252db31933c4f315a8b59688
Aizhu Xue <aizhudeveloper@gmail.com>
2024-12-21 06:16:43 -0500

    add  getfromarray

--- index31.js
+++ index31.js
@@ -43,3 +43,22 @@ function apple(x) {
     ? "It's hotter than the sun!!"
     : "Help yourself to a honeycomb Yorkie for the glovebox.";
 }
+
+function getLength(arr) {
+  return arr.length;
+}
+function getFirst(arr) {
+  return arr[0];
+}
+function getLast(arr) {
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  var el = 1;
+  arr.push(el);
+  return arr;
+}
+function popElement(arr) {
+  arr.pop();
+  return arr;
+}
