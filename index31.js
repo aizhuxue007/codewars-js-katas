0f6704160c26eb75226b7adf46880150846a4342
Aizhu Xue <aizhudeveloper@gmail.com>
2024-12-28 08:19:06 -0500

    add grabdoll

--- index31.js
+++ index31.js
@@ -87,3 +87,18 @@ const a = [];
 function add(a, b) {
   return a == b;
 }
+
+function grabDoll(dolls) {
+  var bag = [];
+
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] === "Hello Kitty" || dolls[i] === "Barbie doll") {
+      bag.push(dolls[i]);
+    } else {
+      continue;
+    }
+    if (bag.length >= 3) break;
+  }
+
+  return bag;
+}
