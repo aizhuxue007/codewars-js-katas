3b03866dba75803e4af387c67a120e4a81a25e01
Aizhu Xue <aizhudeveloper@gmail.com>
2024-12-31 10:47:34 -0500

    add roundIt

--- index32.js
+++ index32.js
@@ -7,3 +7,10 @@ function colorOf(r, g, b) {
     .toString(16)
     .padStart(2, "0")}${b.toString(16).padStart(2, "0")}`;
 }
+
+function roundIt(n) {
+  const nArray = String(n).split(".");
+  if (nArray[0].length < nArray[1].length) return Math.ceil(n);
+  if (nArray[0].length > nArray[1].length) return Math.floor(n);
+  return Math.round(n);
+}
