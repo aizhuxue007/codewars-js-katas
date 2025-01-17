7bc298232d6fe6d8a95776d272dfd1c5bedc0342
Aizhu Xue <aizhudeveloper@gmail.com>
2025-01-17 05:56:50 -0500

    add stray

--- index33.js
+++ index33.js
@@ -1,3 +1,16 @@
 var number = function (array) {
   return array.map((str, i) => `${i + 1}: ${str}`);
 };
+
+function stray(numbers) {
+  const [a, b] = [...new Set(numbers)];
+  const aOccurence = numbers.reduce(
+    (acc, val) => (val === a ? (acc += 1) : (acc += 0)),
+    0
+  );
+  const bOccurence = numbers.reduce(
+    (acc, val) => (val === b ? (acc += 1) : (acc += 0)),
+    0
+  );
+  return aOccurence < bOccurence ? a : b;
+}
