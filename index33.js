bbeaf0173da922f2c29cc83cfcb070809ee7f319
Aizhu Xue <aizhudeveloper@gmail.com>
2025-01-28 06:37:06 -0500

    add declareWinner

--- index33.js
+++ index33.js
@@ -81,3 +81,14 @@ function solve(s) {
     .filter((c) => c === c.toLowerCase()).length;
   return upperCaseCount > lowerCaseCount ? s.toUpperCase() : s.toLowerCase();
 }
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  const firstFighter = fighter1.name === firstAttacker ? fighter1 : fighter2;
+  const secondFighter = fighter1.name !== firstAttacker ? fighter1 : fighter2;
+  while (fighter1.health > 0 && fighter2.health > 0) {
+    secondFighter.health -= firstFighter.damagePerAttack;
+    if (secondFighter.health <= 0) return firstFighter.name;
+    firstFighter.health -= secondFighter.damagePerAttack;
+    if (firstFighter.health <= 0) return secondFighter.name;
+  }
+}
