65fb0fced3c6ea2696d918b97b38f63d3bbb1004
Aizhu Xue <aizhudeveloper@gmail.com>
2025-02-01 07:16:17 -0500

    add capitalize

--- index34.js
+++ index34.js
@@ -1,3 +1,16 @@
 function minValue(values) {
   return +[...new Set(values)].sort().join("");
 }
+
+function capitalize(s) {
+  return [
+    s
+      .split("")
+      .map((c, i) => (i % 2 === 0 ? c.toUpperCase() : c))
+      .join(""),
+    s
+      .split("")
+      .map((c, i) => (i % 2 === 1 ? c.toUpperCase() : c))
+      .join(""),
+  ];
+}
