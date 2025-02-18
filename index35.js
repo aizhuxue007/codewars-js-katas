fbf87a934415f0b2f7942bbb7baff827cb3474fb
Aizhu Xue <aizhudeveloper@gmail.com>
2025-02-17 19:24:09 -0500

    add sum

--- index35.js
+++ index35.js
@@ -37,3 +37,11 @@ function fizzbuzz(n)
 function digits(n) {
     return String(n).length;
 }
+
+function sumCubes(n){
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i**3;
+      }
+    return sum;
+}
