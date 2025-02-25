e5dff3c607d83ceb97abbe6fa7dd099fdf51ff38
Aizhu Xue <aizhudeveloper@gmail.com>
2025-02-25 07:58:11 -0500

    add findNum

--- index36.js
+++ index36.js
@@ -6,3 +6,7 @@ function reverseNumber(n) {
     return Math.sign(n)*Number(n.toString().split('').filter(c => c !== '-').reverse().
                   join(''));
 }
+
+function filterString(value) {
+    return +value.split('').filter(c => !isNaN(c)).join('');
+}
