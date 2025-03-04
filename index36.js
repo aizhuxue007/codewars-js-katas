278e8e040397e0370a05f198f4ee50c519a9350b
Aizhu Xue <aizhudeveloper@gmail.com>
2025-03-04 07:47:58 -0500

    add min-max

--- index36.js
+++ index36.js
@@ -33,3 +33,7 @@ function isRubyComing(list) {
 }
 
 const solve = arr => [...new Set(arr.reverse())].reverse();
+
+function maxDiff(list) {
+    return list.length === 0 ? 0 : Math.max(...list) - Math.min(...list);
+};
