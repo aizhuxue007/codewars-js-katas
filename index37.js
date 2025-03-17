bb46740fc0fcdb0631c8b5d12753bdd2d48612ee
Aizhu Xue <aizhudeveloper@gmail.com>
2025-03-17 18:15:02 -0400

    add simple-characters

--- index37.js
+++ index37.js
@@ -1,7 +1,14 @@
-
 function tidyNumber(n){
     const nSplit = String(n).split('');
     const a = +nSplit.join('');
     const b = +nSplit.sort((a, b) => a - b).join('');
     return a === b;
+}
+
+function solve(s){
+   if (s === '') return [0, 0, 0, 0];
+   return [s.match(/[A-Z]/g)?.length || 0, 
+           s.match(/[a-z]/g)?.length || 0, 
+           s.match(/[0-9]/g)?.length || 0, 
+           s.match(/[^A-Za-z0-9]/g)?.length || 0];
 }
