9cc6d5a0c338a47da849d161a0d1998e83ab547c
Aizhu Xue <aizhudeveloper@gmail.com>
2025-04-02 07:40:04 -0400

    add calc

--- index37.js
+++ index37.js
@@ -80,3 +80,18 @@ function removeRotten(bagOfFruits){
     if (!bagOfFruits || bagOfFruits.length < 1) return [];
     return bagOfFruits.map(fruit => fruit.split('rotten').join('').toLowerCase());
 }
+
+function calculate(num1, operation, num2) {
+    switch (operation) {
+          case "+":
+                  return num1 + num2;
+                        case "-":
+                                return num1 - num2;
+                                      case "*":
+                                              return num1*num2;
+                                                    case "/":
+                                                            return num2 !== 0 ? num1/num2 : null;
+                                                                  default:
+                                                                          return null;
+                                                                            }
+}
